05ae80e86dfcd936a09da1698dcab4ba35d0be6d
davesheils <davesheils@gmail.com>
2019-12-11 18:51:11 +0000

    added updated files, inlcude DAO and code to populate database

--- application.py
+++ application.py
@@ -6,40 +6,9 @@
 
 from flask import Flask, jsonify, request, abort, make_response, render_template
 import json
+from StockDAO import stockDAO
 
-stock = [
-    {
-        "id":1,
-        "Type":"Book",
-        "Title":"Book 1",
-        "Artist_Author":"Author 1",
-        "Genre":"Fiction",
-        "Quantity":"10",
-        "Price":10,
-        "Discogs_GoodReadsID":"12345"
-    },
-    {
-        "id":2,
-        "Type":"CD",
-        "Title":"CD 1",
-        "Artist_Author":"Band 1",
-        "Genre":"Punk Rockers",
-        "Quantity":"10",
-        "Price":11,
-        "Discogs_GoodReadsID":"678910"
-    },
-    {
-        "id":3,
-        "Type":"LP",
-        "Title":"LP 1",
-        "Artist_Author":"Band 1",
-        "Genre":"Roots Rockers",
-        "Quantity":"10",
-        "Price":12,
-        "Discogs_GoodReadsID":"234354"
-    }]
-
-nextID = 4
+# from flask_cors import CORS # review if this is necessary
 
 app = Flask(__name__, static_url_path='',static_folder = '.')
 
@@ -53,35 +22,35 @@ def home():
 # READ with ['GET']
 @app.route('/stock', methods=['GET'])
 def getAll():
-    ##  stock = stockDAO.getAll()
+    stock = stockDAO.getAll()
     return jsonify(stock) #jsonify comes from flask. compare with week 5, w
 # curl -i http://localhost:5000/stock
 
 # Find stock item by ID
-@app.route('/stock/<int:id>', methods =['GET', 'POST', 'PUT'])
+@app.route('/stock/<int:id>',  methods =['GET', 'POST', 'PUT'])
 def findByID(id):
-    foundItem = list(filter(lambda t : t['id'] == id , stock))
-    if len(foundItem) == 0:
-        return jsonify( { 'stock' : '' }),204
-    return jsonify( { 'stock' : foundItem[0] })
+    foundItem =stockDAO.getByID(id)
+    return jsonify(foundItem)
 #    curl -i http://127.0.0.1:5000/stock/5
     
 # CREATE with ['POST']
 @app.route('/stock', methods = ['POST'])
 def create():
-    global nextID
     if not request.json:
         abort(400)
-    item = {"Type":request.json['Type'],
-           "Title":request.json['Title'],
-            "Artist_Author":request.json['Artist_Author'],
-            "Genre":request.json['Genre'],
-            "Quantity":request.json['Quantity'],
-            "Price":request.json['Price'],
-            "Discogs_GoodReadsID":request.json['Discogs_GoodReadsID']
-            }
-    item["id"] = nextID
-    nextID += 1
+    # item = {"Type":request.json['Type'],
+    #        "Title":request.json['Title'],
+    #        "Artist_Author":request.json['Artist_Author'],
+    #        "Genre":request.json['Genre'],
+    #        "Quantity":request.json['Quantity'],
+    #        "Price":request.json['Price']
+    #        # "Discogs_GoodReadsID":request.json['Discogs_GoodReadsID'],
+    #        }
+    # values = (item['Type'],item['Title'],item['Artist_Author'],item['Genre'],item['Quantity'],item['Price'])
+    item = request.json
+    values = tuple(list(item.values()))
+    newID = stockDAO.create(values)
+    item['id'] = newID
     return jsonify(item)
 
     
@@ -89,10 +58,7 @@ def create():
 # UPDATE with ['PUT']
 @app.route('/stock/<int:id>', methods = ['PUT'])
 def update(id):
-    foundItem = list(filter(lambda t : t['id'] == id , stock))
-    if len(foundItem) == 0:
-        return jsonify( { 'stock' : '' }),204
-    
+    foundItem = stockDAO.getByID(id)
     if not foundItem:
         abort(404)
     if not request.json:
@@ -117,14 +83,14 @@ def update(id):
         
     # values = tuple(list(foundItem.values())) # not sure why this does not work here ...
      
-    # stockDAO.update(values)nextID
+    stockDAO.update(values)
     return jsonify(foundItem)
 
 
 # DELETE with ['DELETE']
 @app.route('/stock/<int:id>', methods = ['DELETE'])
 def delete(id):
-    stock.delete(id)
+    stockDAO.delete(id)
     return jsonify( {'result':'true'})
     # example of delete with curl
     # curl -X DELETE http://davidsheils.pythonanywhere.com/stock/2
