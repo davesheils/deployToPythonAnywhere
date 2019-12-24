f39d7156749c70d52cefc1f6d44d74d79de834e3
davesheils <davesheils@gmail.com>
2019-12-24 19:09:25 +0000

    amendment to DAO

--- StockDAO.py
+++ StockDAO.py
@@ -11,12 +11,12 @@ class StockDAO:
     def initConnectToDB(self):
         try:
             db = mysql.connector.connect(
-    		'host'='davidsheils.mysql.pythonanywhere-services.com',
-    		'user'='davidsheils',
-    		'password'='vqx92ffp',
-    		'database'='davidsheils$shop',
+    		host ='davidsheils.mysql.pythonanywhere-services.com',
+    		user ='davidsheils',
+    		password ='vqx92ffp',
+    		database ='davidsheils$shop',
                 pool_name = 'my_connection_pool',
-                pool_size = 6
+                pool_size = 10
                 # auth_plugin='mysql_native_password
                 )
             return db
