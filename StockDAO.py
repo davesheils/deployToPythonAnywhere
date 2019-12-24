d6310df469b800aa86139f50a155b8a70bda564a
davesheils <davesheils@gmail.com>
2019-12-24 19:07:02 +0000

    amendment to DAO

--- StockDAO.py
+++ StockDAO.py
@@ -14,7 +14,7 @@ class StockDAO:
     		'host'='davidsheils.mysql.pythonanywhere-services.com',
     		'user'='davidsheils',
     		'password'='vqx92ffp',
-    		'database'='davidsheils$shop'
+    		'database'='davidsheils$shop',
                 pool_name = 'my_connection_pool',
                 pool_size = 6
                 # auth_plugin='mysql_native_password
